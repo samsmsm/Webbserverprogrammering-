30a5f2b6b4996bc15561cb4e325f8fbba56a4985
Sam <sam.gagiu.te16c@gmail.com>
2017-12-05 16:39:37 +0100

    skapade ett basic formulär

---
+++ uppgifter/login/login.php
@@ -0,0 +1,18 @@
+ 	</head>
+ 
+ 	<body>
+	
+		<form
+	
+	method="POST" action="submit.php">
+	
+		Användarnamn: <input type="text" name="Anvandarnamn" </input>
+		<br>
+		<br>
+		Lösenord: <input type="password" name="Losenord" </input>
+		
+		<input type="submit">
+		</form>
+ 
+ 
+ 	</body>
