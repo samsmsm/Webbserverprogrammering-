783ac27b57269ccd9ff8827ddb9d407d9763c684
Sam <sam.gagiu.te16c@gmail.com>
2017-12-05 16:41:53 +0100

    Skapade en databas

---
+++ uppgifter/register/register.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE php;
+
+USE php;
+
+
+CREATE TABLE register(id int(8) PRIMARY KEY AUTO_INCREMENT, Anvandarnamn varchar(32), Fullstandigtnamn varchar(32), Mail varchar(32), Losenord varchar(32), Fodelsedatum varchar(32);
