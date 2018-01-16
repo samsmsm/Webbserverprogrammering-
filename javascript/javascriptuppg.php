9cd4f818d7812c7ff5e441b0d07f6b36d211b90f
Sam <sam.gagiu.te16c@gmail.com>
2018-01-16 16:07:20 +0100

    Alerta ett valfritt meddelande om man klickar på en <button>

---
+++ javascript/javascriptuppg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "FAKE QUEEN, SPIT ON HER!!!";
+}
+</script>
+</head>
+
+<body>
+
+<h2>Alerta ett valfritt meddelande om man klickar på en.</h2>
+
+<p id="demo">DO YOU KNO DE WEY?</p>
+
+<button type="button" onclick="myFunction()">Show</button>
+
+</body>
+</html> 
