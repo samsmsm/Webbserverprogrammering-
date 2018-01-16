520328192a473342f61ce6deb23e9152bd4db8e6
Sam <sam.gagiu.te16c@gmail.com>
2018-01-16 22:02:09 +0100

    Skapa en div som bara ska innehålla siffran 0. Skapa sedan 3 st <button> där den ena knappen ökar värdet på siffran i diven med 1, den andra knappen ska öka värdet med 10 och den sista öka värdet med 100.

--- javascript/javascriptuppg.php
+++ javascript/javascriptuppg.php
@@ -1,22 +1,30 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML>
+<HTML>
 <head>
-<script>
-function myFunction() {
-    document.getElementById("demo").innerHTML = "You cleared the page :/";
+<style>
+#clear{
+	width: 100px;
+	
+}
+</style>
+<script type="text/javascript">
+function displynum(n1)
+{
+calcform.txt1.value=calcform.txt1.value+n1;
 }
 </script>
 </head>
-
 <body>
+<form name=calcform>
+<input type=text name=txt1  style="text-align:right"><br>
+<input type=button name=btn9 value=1 onclick="displynum(btn9.value)">
+<input type=button name=btn8 value=10 onclick="displynum(btn8.value)">
+<input type=button name=btn7 value=100 onclick="displynum(btn7.value)">
+<input type=button name=addbtn value=+ onclick="displynum(addbtn.value)"><br>
+<input type=button name=eqlbtn value== onclick="txt1.value=eval(txt1.value)">
 
-<p id="demo"> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Ugandan Knuckles <br><br><br><br> About <br><br> Ugandan Knuckles is the nickname given to a depiction of the character Knuckles <br> from the Sonic franchise created by YouTuber Gregzilla, which is often used <br> as an avatar by players in the multiplayer game VRChat who repeat phrases like <br> “do you know the way” and memes associated with the country Uganda, most <br> notably the film Who Killed Captain Alex? and Zulul. The character is associated <br> with the expression “do you know the way”, which is typically spoken in a mock <br> African accent and phonetically spelled as “do you know de wey.”
-<br><br>
-Origin <br><br> On February 20th, 2017, YouTuber Gregzilla uploaded a review of the 2013 <br> platformer game Sonic Lost World, which featured a parody animation of the <br> character Knuckles (shown below, left). The video subsequently gave rise <br> to the Knuckles Sings remix series.</p>
 
 
-
-<button type="button" onclick="myFunction()">Clear Page</button>
-
+</form>
 </body>
-</html> 
+</html>﻿
