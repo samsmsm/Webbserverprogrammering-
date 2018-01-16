77677a9df277da8c1a53f0c85e0dd9db7d387e91
Sam <sam.gagiu.te16c@gmail.com>
2018-01-16 16:23:24 +0100

    Lägg till en bild som blir större när man hovrar över den.

--- javascript/javascriptuppg.php
+++ javascript/javascriptuppg.php
@@ -1,20 +1,39 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
-<script>
-function myFunction() {
-    document.getElementById("demo").innerHTML = "FAKE QUEEN, SPIT ON HER!!!";
+<title> Untitled </title>
+<script type="text/javascript">
+function changeImg(who,flag) {
+  if (flag) {
+    who.style.height='125px';  who.style.width='100px';
+  } else {
+    who.style.height='100px';  who.style.width='75px';
+  }
 }
 </script>
+
+<style type="text/css">
+
+.imgSize {
+
+ height:100px; width:75px;
+
+ vertical-align:bottom;
+
+}
+
+</style>
+
+ 
+
 </head>
 
 <body>
 
-<h2>Alerta ett valfritt meddelande om man klickar på en.</h2>
+<img src="http://www.nova.edu/hpd/otm/pics/4fun/11.jpg" class="imgSize"
 
-<p id="demo">DO YOU KNO DE WEY?</p>
-
-<button type="button" onclick="myFunction()">Show</button>
+ onmouseover="changeImg(this,true)" onmouseout="changeImg(this,false)">
 
 </body>
-</html> 
+
+</html>
