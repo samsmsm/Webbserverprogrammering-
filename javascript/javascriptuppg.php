9f58c1d8cf02d54891041a1c0d9e184a60eabf66
Sam <sam.gagiu.te16c@gmail.com>
2018-01-16 19:58:50 +0100

    Skapa en <button> som rensar hela webbsidan och istället lämnar kvar en valfri text.

--- javascript/javascriptuppg.php
+++ javascript/javascriptuppg.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<h1>Ändrar bakgrundsfärg!</h1>
-
-<button type="button" onclick="myFunction()">Byt bakrundsfärg</button>
-
+<head>
 <script>
 function myFunction() {
-    document.body.style.backgroundColor = "red";
+    document.getElementById("demo").innerHTML = "You cleared the page :/";
 }
 </script>
+</head>
+
+<body>
+
+<p id="demo"> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Ugandan Knuckles <br><br><br><br> About <br><br> Ugandan Knuckles is the nickname given to a depiction of the character Knuckles <br> from the Sonic franchise created by YouTuber Gregzilla, which is often used <br> as an avatar by players in the multiplayer game VRChat who repeat phrases like <br> “do you know the way” and memes associated with the country Uganda, most <br> notably the film Who Killed Captain Alex? and Zulul. The character is associated <br> with the expression “do you know the way”, which is typically spoken in a mock <br> African accent and phonetically spelled as “do you know de wey.”
+<br><br>
+Origin <br><br> On February 20th, 2017, YouTuber Gregzilla uploaded a review of the 2013 <br> platformer game Sonic Lost World, which featured a parody animation of the <br> character Knuckles (shown below, left). The video subsequently gave rise <br> to the Knuckles Sings remix series.</p>
+
+
+
+<button type="button" onclick="myFunction()">Clear Page</button>
 
 </body>
-</html>
+</html> 
